22bd9ab5b69ebd6d671d80eb832c2e702a2ef49f
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2024-02-04 16:09:43 +0100

    Intialize repository.

---
+++ worker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+SCRIPT_PATH="$(dirname "$(realpath "$0")")"
+
+cd "$SCRIPT_PATH"
+
+echo '<!DOCTYPE html>' > index.html
+echo '<html lang="en-US">' >> index.html
+cat res/head.html >> index.html
+
+echo '<body>' >> index.html
+markdown README.md >> index.html
+echo '</body>' >> index.html
+echo '</html>' >> index.html
+
+echo 'Executing setup...'
+sh scripts/set.sh res/parser.conf index.html
+sh scripts/set.sh res/parser.conf README.md
+
+rm -f .gitignore
+
